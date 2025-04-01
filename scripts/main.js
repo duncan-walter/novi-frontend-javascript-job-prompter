8ba202979e66215ae4cc808b2f56cc6759f0d9e3
Duncan Walter <duncan1997@hotmail.nl>
2025-04-01 16:23:16 +0200

    finished exercise 1

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
-console.log(departments);
+// Opdracht 1a: Log het aantal medewerkers van de sales-afdeling in de console, in het volgende format: "De afdeling Sales heeft [x] medewerkers"
+console.log(`De afdeling Sales heeft ${departments.sales.numberOfEmployees} medewerkers.`);
+
+// Opdracht 1b: Log de afdeling-beschrijving van de marketingafdeling in de console, in het volgende format: " Marketing is een leuke afdeling om te werken. [beschrijving]."
+console.log(`Marketing is een leuke afdeling om te werken. ${departments.marketing.description}.`);
+
+// Opdracht 1c: Log het aantal medewerkers van de customer-service-afdeling in de console, in het volgende format: "De afdeling Customer Service heeft [aantal] medewerkers" Let op: er is iets raars aan de hand met deze property!
+console.log(`De afdeling Customer Service heeft ${departments["customer-service"].numberOfEmployees} medewerkers.`);
+
+// Opdracht 1d: Log de beschrijving van de functie "Verkoopmanager" in de console, in het volgende format: "Sales is een uitdagende afdeling om te werken als Verkoopmanager. [beschrijving functie Verkoopmanager]"
+console.log(`Sales is een uitdagende afdeling om te werken als Verkoopmanager. ${departments.sales.jobs[1].description}.`);
