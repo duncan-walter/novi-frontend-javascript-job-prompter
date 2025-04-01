211380d8642b19011f2ca65d06d41d9e636ce8cc
Duncan Walter <duncan1997@hotmail.nl>
2025-04-01 17:00:22 +0200

    finished exercise 4

--- scripts/main.js
+++ scripts/main.js
@@ -11,22 +11,22 @@ console.log(`De afdeling Customer Service heeft ${departments["customer-service"
 console.log(`Sales is een uitdagende afdeling om te werken als Verkoopmanager. ${departments.sales.jobs[1].description}.`);
 
 
-// // Opdracht 2a: Gebruik bovenstaand voorbeeld en pas het zo aan dat de browser jou de volgende vraag stelt: Over welke afdeling wil je meer informatie? Kies uit: [marketing / sales / customer-service]. Het antwoord dat jij invoert, log je uiteraard in de console. Tip: geef jouw input-prompt altijd in kleine letters ("marketing" in plaats van "Marketing" of "MARKETING"), dit voorkomt problemen in het script.
-// const userInput = prompt("Over welke afdeling wil je meer informatie? Kies uit: [marketing / sales / customer-service]").toLowerCase();
-// console.log(userInput);
-//
-// // Opdracht 2b: tijd voor het echte werk! Op basis van wat de gebruiker invoert ("marketing", "sales" of "customer service") willen wij een beschrijving van die afdeling in de console loggen. Hiervoor typ je natuurlijk niet handmatig de beschrijvingen over! Je spreekt ze aan via het departments-object: hiervoor zul je dus een beslissingsstructuur moeten bouwen. Ongeacht de gekozen afdeling, log je de uitkomst in het volgende format: "Je koos [ingevoerde keuze]. [beschrijving afdeling]"
-// // Opdracht 2c: test of alles werkt door alle mogelijke afdelingen een keertje te voeren en te checken of je de gewenste output krijgt. Maak jouw beslisboom nu extra gebruiksvriendelijk door de melding "Ongeldige keuze. Probeer het opnieuw door de pagina te verversen." in de console te laten zien wanneer de gebruiker een spelfout maakt of iets verkeerds invoert. Tip: je kunt hiervoor console.error() gebruiken in plaats van console.log().
-// switch (userInput) {
-//     case "marketing":
-//     case "sales":
-//     case "customer-service":
-//         console.log(`Je koos ${userInput}. ${departments[userInput].description}`);
-//         break;
-//     default:
-//         console.error("Ongeldige keuze.");
-//         break;
-// }
+// Opdracht 2a: Gebruik bovenstaand voorbeeld en pas het zo aan dat de browser jou de volgende vraag stelt: Over welke afdeling wil je meer informatie? Kies uit: [marketing / sales / customer-service]. Het antwoord dat jij invoert, log je uiteraard in de console. Tip: geef jouw input-prompt altijd in kleine letters ("marketing" in plaats van "Marketing" of "MARKETING"), dit voorkomt problemen in het script.
+const userDepartmentInput = prompt("Over welke afdeling wil je meer informatie? Kies uit: [marketing / sales / customer-service]").toLowerCase();
+console.log(userDepartmentInput);
+
+// Opdracht 2b: tijd voor het echte werk! Op basis van wat de gebruiker invoert ("marketing", "sales" of "customer service") willen wij een beschrijving van die afdeling in de console loggen. Hiervoor typ je natuurlijk niet handmatig de beschrijvingen over! Je spreekt ze aan via het departments-object: hiervoor zul je dus een beslissingsstructuur moeten bouwen. Ongeacht de gekozen afdeling, log je de uitkomst in het volgende format: "Je koos [ingevoerde keuze]. [beschrijving afdeling]"
+// Opdracht 2c: test of alles werkt door alle mogelijke afdelingen een keertje te voeren en te checken of je de gewenste output krijgt. Maak jouw beslisboom nu extra gebruiksvriendelijk door de melding "Ongeldige keuze. Probeer het opnieuw door de pagina te verversen." in de console te laten zien wanneer de gebruiker een spelfout maakt of iets verkeerds invoert. Tip: je kunt hiervoor console.error() gebruiken in plaats van console.log().
+switch (userDepartmentInput) {
+    case "marketing":
+    case "sales":
+    case "customer-service":
+        console.log(`${userDepartmentInput} is een leuke afdeling om te werken. Er werken op dit moment ${departments[userDepartmentInput].numberOfEmployees} medewerkers.`);
+        break;
+    default:
+        console.error("Ongeldige keuze.");
+        break;
+}
 
 // Opdracht 3a: We gaan er voor nu even vanuit dat de gebruiker de afdeling 'marketing' heeft gekozen. Schrijf een nieuwe prompt die de gebruiker om het volgende vraagt:
 //
@@ -37,11 +37,11 @@ console.log(`Sales is een uitdagende afdeling om te werken als Verkoopmanager. $
 // 3: [functietitel marketing 3]
 //
 // Hierbij typ je de functietitels van Marketing niet handmatig over, maar spreek je ze aan via de marketing-property in het departments-object.
-const userJobInput = prompt("Je koos marketing. Over welke functie wil je meer weten? Voer eem getal tussen 0 en 3 in." +
-    `\n0: ${departments.marketing.jobs[0].title}` +
-    `\n1: ${departments.marketing.jobs[1].title}` +
-    `\n2: ${departments.marketing.jobs[2].title}` +
-    `\n3: ${departments.marketing.jobs[3].title}`)
+const userJobInput = prompt(`Je koos ${userDepartmentInput}. Over welke functie wil je meer weten? Voer een getal tussen 0 en 3 in.` +
+    `\n0: ${departments[userDepartmentInput].jobs[0].title}` +
+    `\n1: ${departments[userDepartmentInput].jobs[1].title}` +
+    `\n2: ${departments[userDepartmentInput].jobs[2].title}` +
+    `\n3: ${departments[userDepartmentInput].jobs[3].title}`)
 
 // Opdracht 3b: Op basis van wat de gebruiker invoert (0, 1, 2, of 3) willen wij de beschrijving van de bijbehorende marketing-functie-titel en marketing-functie-beschrijving in de console loggen. Dit doe je in het volgende format:
 //
@@ -54,9 +54,12 @@ switch (userJobInput) {
     case "1":
     case "2":
     case "3":
-        console.log(`Je koos ${departments.marketing.jobs[userJobInput].title}. Een uitdagende rol! ${departments.marketing.jobs[userJobInput].description}.`);
+        console.log(`Je koos ${departments[userDepartmentInput].jobs[userJobInput].title}.` +
+            `Een uitdagende rol! ${departments[userDepartmentInput].jobs[userJobInput].description}.`);
         break;
     default:
         console.error("Ongeldige keuze.");
         break;
 }
+
+// De opdrachten die onder opdracht 4 vallen zijn hierboven verwerkt in opdracht 2 en 3.
